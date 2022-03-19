a57a51f09e0e72c83c620451c52aa081f6e5bba2
EduardooPV <eduardoveltroni@hotmail.com>
2022-03-19 18:37:21 -0300

    Feat: adiciona funcionalidade de decanso

---
+++ Todos.md
@@ -0,0 +1,11 @@
+# To-do das funcionalidades:
+
+- [x] Contador regressivo
+- [x] Notificação
+- [x] Tempo de descanso (5 minutos)
+- [ ] Refatoração
+- [ ] Autenticação
+- [ ] Rank local com localStorage (JavaScript API)
+- [ ] Refatoração
+- [ ] Possibilidade de personalizar o app (Cores/fonts)
+- [ ] Dark-mode
--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -8,6 +8,7 @@ export function Countdown() {
 
   const [time, setTime] = useState(25 * 60)
   const [isActive, setIsActive] = useState(false)
+  const [rest, setRest] = useState(false)
 
   const minutes = Math.floor(time / 60)
   const seconds = time % 60
@@ -27,47 +28,79 @@ export function Countdown() {
   function resetCountdown() {
     clearTimeout(countdownTimeout)
     setIsActive(false)
-    setTime(25 * 60)
+    if (rest) {
+      setTime(5 * 60)
+    } else {
+      setTime(25 * 60)
+    }
   }
 
   useLayoutEffect(() => {
-    if (isActive && time > 0) {
-      countdownTimeout = setTimeout(() => {
-        setTime(time - 1)
+    if (rest) {
+      if (isActive && time > 0) {
+        countdownTimeout = setTimeout(() => {
+          setTime(time - 1)
+        }, 1000)
+      } else if (isActive && time === 0) {
+        alertNotify('Acabou o descanso!', 'Bora voltar para a luta!')
+        setIsActive(false)
+        setTime(25 * 60)
+        setRest(false)
+      }
+    } else {
+      if (isActive && time > 0) {
+        countdownTimeout = setTimeout(() => {
+          setTime(time - 1)
 
-        if (time === 5 * 60) {
-          alertNotify(
-            'Restam 5 minutos',
-            'Prepare-se para a revisão do estudo.'
-          )
-        }
-      }, 1000)
-    } else if (isActive && time === 0) {
-      alertNotify('Acabou!', 'Parabéns, você finalizou essa sprint!')
-      setIsActive(false)
+          if (time === 5 * 60) {
+            alertNotify(
+              'Restam 5 minutos',
+              'Prepare-se para a revisão do estudo.'
+            )
+          }
+        }, 1000)
+      } else if (isActive && time === 0) {
+        alertNotify('Acabou!', 'Parabéns, você finalizou essa sprint!')
+        setIsActive(false)
+        setTime(0.1 * 60)
+        setRest(true)
+      }
     }
   }, [isActive, time])
+
+  // setRest(true)
+  // setTime(5 * 60)
 
   function alertNotify(title: string, body: string) {
     // eslint-disable-next-line no-new
     new Notification(title, { body: body, icon: PomoIcon })
   }
-  
+
   return (
     <Container>
       <h1>Pomoduds</h1>
 
-      <Quantity>
-        <button onClick={() => setTime(time > 0 ? time - 5 * 60 : 0)}>-</button>
-        <button onClick={() => setTime(20 * 60)}>20</button>
-        <button onClick={() => setTime(25 * 60)}>25</button>
-        <button onClick={() => setTime(30 * 60)}>30</button>
-        <button
-          onClick={() => setTime(time < 60 * 60 ? time + 5 * 60 : 60 * 60)}
-        >
-          +
-        </button>
-      </Quantity>
+      {rest ? (
+        <Quantity>
+          <button onClick={() => setTime(5 * 60)}>5</button>
+          <button onClick={() => setTime(10 * 60)}>10</button>
+          <button onClick={() => setTime(15 * 60)}>15</button>
+        </Quantity>
+      ) : (
+        <Quantity>
+          <button onClick={() => setTime(time > 0 ? time - 5 * 60 : 0)}>
+            -
+          </button>
+          <button onClick={() => setTime(20 * 60)}>20</button>
+          <button onClick={() => setTime(25 * 60)}>25</button>
+          <button onClick={() => setTime(30 * 60)}>30</button>
+          <button
+            onClick={() => setTime(time < 60 * 60 ? time + 5 * 60 : 60 * 60)}
+          >
+            +
+          </button>
+        </Quantity>
+      )}
 
       <Timer>
         <span>{minuteLeft}</span>
